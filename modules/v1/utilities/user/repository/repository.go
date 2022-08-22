0b93e94dec0dc8f5436455daee6e340a72416ae1
Rizqi Wijaya <rizwijaya@github.com>
2022-08-22 12:25:17 +0700

    Create Service and Repository for Register Features.

--- modules/v1/utilities/user/repository/repository.go
+++ modules/v1/utilities/user/repository/repository.go
@@ -1,16 +1,248 @@
 package repository
 
 import (
+	"context"
+	"e-signature/app/blockhain"
+	"e-signature/app/config"
+	api "e-signature/app/contracts"
+	"e-signature/modules/v1/utilities/user/models"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"gorm.io/gorm"
 )
 
 type Repository interface {
+	SearchFile(path string, info os.FileInfo, err error) error
+	GetPrivateKey(user models.User) (string, error)
+	GeneratePublicKey(user models.User) (models.User, error)
+	//SavetoSystem(auth *bind.TransactOpts, user models.User) error
+	Register(user models.User) error
+	SavetoProfile(user models.User, key string) error
+	CheckUserExist(idsignature string) (models.ProfileDB, error)
+	TransferBalance(user models.User) (string, error)
 }
 
 type repository struct {
-	db *gorm.DB
+	db         *gorm.DB
+	blockchain *api.Api
+	client     *ethclient.Client
 }
 
-func NewRepository(db *gorm.DB) *repository {
-	return &repository{db}
+func NewRepository(db *gorm.DB, blockchain *api.Api, client *ethclient.Client) *repository {
+	return &repository{db, blockchain, client}
+}
+
+var filesPrivate []string
+
+func (r *repository) SearchFile(path string, info os.FileInfo, err error) error {
+	var user models.User
+	pub := user.Publickey
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	reg, err2 := regexp.Compile(pub + "$")
+	if err2 != nil {
+		log.Println(err2)
+		return err
+	}
+
+	if reg.MatchString(info.Name()) {
+		filesPrivate = append(filesPrivate, path)
+	}
+
+	return nil
+}
+
+func (r *repository) GetPrivateKey(user models.User) (string, error) {
+	err := filepath.Walk("./app/account", r.SearchFile)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	var filePrivate string
+	for _, file := range filesPrivate {
+		filePrivate = file
+	}
+	if len(filePrivate) == 0 {
+		return "", errors.New("file not found")
+	}
+	//Dapatkan Private Key dari File Keystorenya
+	outPath := "./app/tmp/hash.hex"
+	keyjson, e := ioutil.ReadFile(filePrivate)
+	if e != nil {
+		log.Fatal(e)
+		return "", e
+	}
+	key, e := keystore.DecryptKey(keyjson, user.Password)
+	if e != nil {
+		log.Fatal(e)
+		return "", e
+	}
+	//fmt.Println(key.PrivateKey)
+	e = crypto.SaveECDSA(outPath, key.PrivateKey)
+	if e != nil {
+		log.Fatal(e)
+		return "", e
+	}
+	z, _ := ioutil.ReadFile(outPath)
+	//fmt.Println(string(z))
+	os.Remove(outPath)
+
+	return string(z), err
+}
+
+func (r *repository) GeneratePublicKey(user models.User) (models.User, error) {
+	ks := keystore.NewKeyStore("./app/account", keystore.StandardScryptN, keystore.StandardScryptP)
+	password := user.Password
+
+	account, err := ks.NewAccount(password)
+	if err != nil {
+		log.Fatal(err)
+		return user, err
+	}
+	user.Publickey = account.Address.Hex()
+	// file, _ := r.GetPrivateKey()
+
+	// jsonBytes, err := ioutil.ReadFile(file)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return user, err
+	// }
+
+	// account, err = ks.Import(jsonBytes, password, password)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return user, err
+	// }
+
+	// user.Publickey = account.Address.Hex() //save to struct
+	return user, nil
+}
+
+// func (r *repository) SavetoSystem(auth *bind.TransactOpts, user models.User) error {
+// 	_, err := r.blockchain.AddProfilefirst(auth, user.Idsignature, user.Password, user.Publickey, user.Role)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return err
+// 	}
+// 	return nil
+// }
+func (r *repository) Register(user models.User) error {
+	var profile models.ProfileDB
+	profile.Idsignature = user.Idsignature
+	profile.Password = user.PasswordHash
+	profile.Name = user.Name
+	profile.Email = user.Email
+	profile.Phone = user.Phone
+	profile.Identity_card = user.Identity_card
+	profile.PublicKey = user.Publickey
+	profile.Role_id = user.Role
+	err := r.db.Table("users").Create(&profile).Error
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+func (r *repository) SavetoProfile(user models.User, key string) error {
+	//auth := blockhain.GetAccountAuth(blockhain.Connect(), key)
+	auth := blockhain.GetAccountAuth(blockhain.Connect(), key)
+	rs, err := r.blockchain.AddProfile(auth, user.Name, user.ImageIPFS, user.Email, user.Phone, user.Dateregistered)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	fmt.Println(rs)
+	return nil
+}
+
+func (r *repository) CheckUserExist(idsignature string) (models.ProfileDB, error) {
+	var profile models.ProfileDB
+	err := r.db.Table("users").Where("idsignature = ?", idsignature).Find(&profile).Error
+	if err != nil {
+		return profile, err
+	}
+
+	return profile, nil
+}
+
+func (r *repository) TransferBalance(user models.User) (string, error) {
+	conf, _ := config.Init()
+	var data []byte
+	err := filepath.Walk("./app/account", r.SearchFile)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	var filePrivate string
+	for _, file := range filesPrivate {
+		filePrivate = file
+	}
+	if len(filePrivate) == 0 {
+		return "", errors.New("file not found")
+	}
+	//Dapatkan Private Key dari File Keystorenya
+	keyjson, e := ioutil.ReadFile(filePrivate)
+	if e != nil {
+		log.Fatal(e)
+		return "", e
+	}
+	key, e := keystore.DecryptKey(keyjson, user.Password)
+	if e != nil {
+		log.Fatal(e)
+		return "", e
+	}
+	//Transfer Balance
+	from := common.HexToAddress(conf.Blockhain.Secret_key)
+	to := common.HexToAddress(user.Publickey)
+
+	value := big.NewInt(1000)
+	gasLimit := uint64(21000)
+	gasPrice, err := r.client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	nonce, err := r.client.PendingNonceAt(context.Background(), from)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	txs := types.NewTransaction(nonce, to, value, gasLimit, gasPrice, data)
+	chainID, err := r.client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	txs, err = types.SignTx(txs, types.NewEIP155Signer(chainID), key.PrivateKey)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	err = r.client.SendTransaction(context.Background(), txs)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	return txs.Hash().Hex(), err
 }
--- modules/v1/utilities/user/service/service.go
+++ modules/v1/utilities/user/service/service.go
@@ -1,22 +1,87 @@
 package service
 
 import (
-	api "e-signature/app/contracts"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
 	"e-signature/modules/v1/utilities/user/models"
 	"e-signature/modules/v1/utilities/user/repository"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"os"
+
+	shell "github.com/ipfs/go-ipfs-api"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Service interface {
+	ConnectIPFS() *shell.Shell
+	UploadIPFS(path string) (error, string)
+	GetFileIPFS(hash string, output string) (string, error)
 	Login(input models.LoginInput) (models.User, error)
+	CreateAccount(user models.User) error
+	SaveImage(input models.RegisterUserInput, file *multipart.FileHeader) (string, error)
+	CreateKey(key string) []byte
+	Encrypt(data []byte, passphrase string) []byte
+	Decrypt(data []byte, passphrase string) []byte
+	EncryptFile(filename string, passphrase string) error
+	DecryptFile(filename string, passphrase string) error
+	//SavetoSystem(user models.User) error
+	CheckUserExist(idsignature string) (string, error)
 }
 
 type service struct {
 	repository repository.Repository
-	blockhain  *api.Api
 }
 
-func NewService(repository repository.Repository, blockhain *api.Api) *service {
-	return &service{repository, blockhain}
+func NewService(repository repository.Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) ConnectIPFS() *shell.Shell {
+	var sh *shell.Shell
+	sh = shell.NewShell("localhost:5001")
+	return sh
+}
+
+func (s *service) UploadIPFS(path string) (error, string) {
+	//fmt.Printf("Adding %s on IPFS \n", filename)
+	sh := s.ConnectIPFS()
+	f, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		return err, ""
+	}
+	var r = f
+	cid, err := sh.Add(r)
+	if err != nil {
+		log.Println(err)
+		return err, ""
+	}
+	defer f.Close()
+
+	err = os.Remove(path)
+	if err != nil {
+		log.Println("The file could not be removed")
+		return err, ""
+	}
+	return err, cid
+}
+
+func (s *service) GetFileIPFS(hash string, output string) (string, error) {
+	sh := s.ConnectIPFS()
+	outputName := "./public/images/" + output
+	err := sh.Get(hash, outputName)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	return outputName, nil
 }
 
 func (s *service) Login(input models.LoginInput) (models.User, error) {
@@ -24,3 +89,168 @@ func (s *service) Login(input models.LoginInput) (models.User, error) {
 	// password := input.Password
 	return models.User{}, nil
 }
+
+func (s *service) CreateAccount(user models.User) error {
+	//Input Hash Password
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	user.PasswordHash = string(hash)
+	//Generate account public key with passphrase user password
+	user, err = s.repository.GeneratePublicKey(user)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	user.Publickey = string(s.Encrypt([]byte(user.Publickey), user.Password))
+	user.Identity_card = string(s.Encrypt([]byte(user.Identity_card), user.Password))
+	//fmt.Println(string(s.Decrypt([]byte(user.Publickey), user.Password)))
+	//Save to Database
+	err = s.repository.Register(user)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	//Get Private Key user
+	// key, err := s.repository.GetPrivateKey(user)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return err
+	// }
+	//Isi gas dan ether pada account agar dapat melakukan transaksi
+	// tx, err := s.repository.TransferBalance(user)
+	// if err != nil {
+	// 	return err
+	// }
+	// fmt.Println(tx)
+	//Save to Network Blockchain account user
+	// fmt.Println(user) // fmt.Println(key)
+	// err = s.repository.SavetoProfile(user, key)
+	// if err != nil {
+	// 	return err
+	// }
+	//fmt.Println(user)
+	return nil
+}
+
+func (s *service) SaveImage(input models.RegisterUserInput, file *multipart.FileHeader) (string, error) {
+	path := fmt.Sprintf("./public/images/%s-%s", input.IdSignature, file.Filename)
+	//Encrypt file Image with AES and Passphrase password
+	err := s.EncryptFile(path, input.Password)
+	if err != nil {
+		return "", err
+	}
+	//Upload to Network IPFS
+	err, cidr := s.UploadIPFS(path)
+	if err != nil {
+		return "", err
+	}
+	return cidr, nil
+}
+
+func (s *service) CreateKey(key string) []byte {
+	hash := sha256.Sum256([]byte(key))
+	return hash[:]
+}
+
+func (s *service) Encrypt(data []byte, passphrase string) []byte {
+	key := s.CreateKey(passphrase)
+	block, _ := aes.NewCipher(key)
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	base64Cipher := make([]byte, base64.RawStdEncoding.EncodedLen(len(ciphertext)))
+	base64.RawStdEncoding.Encode(base64Cipher, ciphertext)
+	//return string(base64Cipher)
+	return base64Cipher
+}
+
+func (s *service) Decrypt(data []byte, passphrase string) []byte {
+	cipherText := make([]byte, base64.RawStdEncoding.DecodedLen(len(data)))
+	_, err := base64.RawStdEncoding.Decode(cipherText, data)
+	if err != nil {
+		return nil
+	}
+	key := s.CreateKey(passphrase)
+	//key := "791f13d3e2552bcf31c4f8d0e5d6a1ed"
+	//fmt.Println("key", key)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := cipherText[:nonceSize], cipherText[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func (s *service) EncryptFile(filename string, passphrase string) error {
+	b, err := ioutil.ReadFile(filename) //Read the target file
+	if err != nil {
+		fmt.Printf("Unable to open the input file!\n")
+		os.Exit(0)
+		return err
+	}
+	ciphertext := s.Encrypt(b, passphrase)
+	err = ioutil.WriteFile(filename, ciphertext, 0644)
+	if err != nil {
+		fmt.Printf("Unable to create encrypted file!\n")
+		os.Exit(0)
+		return err
+	}
+	//fmt.Println(ciphertext)
+	return nil
+}
+
+func (s *service) DecryptFile(filename string, passphrase string) error {
+	z, err := ioutil.ReadFile(filename)
+	result := s.Decrypt(z, passphrase)
+	//fmt.Printf("Decrypted file was created with file permissions 0777\n")
+	err = ioutil.WriteFile(filename, result, 0777)
+	if err != nil {
+		fmt.Printf("Unable to create decrypted file!\n")
+		os.Exit(0)
+		return err
+	}
+	return nil
+}
+
+func (s *service) CheckUserExist(idsignature string) (string, error) {
+	id, err := s.repository.CheckUserExist(idsignature)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	if id.Email == "" {
+		return "no-exist", nil
+	}
+	return "exist", nil
+}
+
+// func (s *service) SavetoSystem(user models.User) error {
+// 	conf, _ := config.Init()
+// 	//System Authentication
+// 	auth := blockhain.GetAccountAuth(blockhain.Connect(), conf.Blockhain.Secret_key)
+// 	err := s.repository.SavetoSystem(auth, user)
+// 	if err != nil {
+// 		log.Println(err)
+// 		return err
+// 	}
+// 	return nil
+// }
