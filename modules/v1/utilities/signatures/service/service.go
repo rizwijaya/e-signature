3d0afead81679be4bb2c6af407894ac3b5839359
Rizqi Wijaya <rizwijaya@github.com>
2022-09-25 21:20:53 +0700

    Create Base64 to Image png files from signatures pad

--- modules/v1/utilities/signatures/service/service.go
+++ modules/v1/utilities/signatures/service/service.go
@@ -1,10 +1,19 @@
 package service
 
 import (
+	"e-signature/modules/v1/utilities/signatures/models"
 	"e-signature/modules/v1/utilities/signatures/repository"
+	"encoding/base64"
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"os"
+	"strings"
 )
 
 type Service interface {
+	CreateImgSignature(input models.AddSignature) string
 }
 
 type service struct {
@@ -13,4 +22,30 @@ type service struct {
 
 func NewService(repository repository.Repository) *service {
 	return &service{repository}
+}
+
+func (s *service) CreateImgSignature(input models.AddSignature) string {
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(input.Signature))
+	m, formatString, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bounds := m.Bounds()
+	fmt.Println(bounds, formatString)
+
+	//Encode from image format to writer
+	pngFilename := "app/tmp/signature-" + input.Id + ".png"
+	f, err := os.OpenFile(pngFilename, os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	err = png.Encode(f, m)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+	//fmt.Println("Png file", pngFilename, "created")
+	return pngFilename
 }
