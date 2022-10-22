6b1e17d29b02ca8247fad7f2b474a88065f39226
Rizqi Wijaya <rizwijaya@github.com>
2022-10-22 09:46:19 +0700

    Fixing Error Upload document for View Document

--- public/assets/landing/js/mysignatures.js
+++ public/assets/landing/js/mysignatures.js
@@ -1,25 +1,25 @@
 // My Signatures
-var signBox = new SignaturePad(document.getElementById('signbox'), {
-  backgroundColor: 'rgba(255, 255, 255, 0)',
-  penColor: 'rgb(0, 0, 0)'
+var signBox = new SignaturePad(document.getElementById("signbox"), {
+    backgroundColor: "rgba(255, 255, 255, 0)",
+    penColor: "rgb(0, 0, 0)",
 });
 
 function addSign() {
-  var data = signBox.toDataURL('image/png');
-  var img = data.replace(/^data:image\/(png|jpg);base64,/, "");
+    var data = signBox.toDataURL("image/png");
+    var img = data.replace(/^data:image\/(png|jpg);base64,/, "");
 
-  var settings = {
-    "url": "/add-signatures",
-    "method": "POST",
-    "timeout": 0,
-    "headers": {
-      "Content-Type": "text/plain"
-    },
-    "data": "{\r\n    \"unique\": " + user + ",\r\n    \"signature\": \"" + img + "\"\r\n}",
-  };
+    var settings = {
+        url: "/add-signatures",
+        method: "POST",
+        timeout: 0,
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        data: '{\r\n    "unique": ' + user + ',\r\n    "signature": "' + img + '"\r\n}',
+    };
 
-  $.ajax(settings).done(function (response) {
-    //console.log(response);
-    location.reload();
-  });
+    $.ajax(settings).done(function (response) {
+        //console.log(response);
+        location.reload();
+    });
 }
