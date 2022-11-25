a6c1c9417e5b4fcce0aafc59bd56c2e341955189
Rizqi Wijaya <rizwijaya@github.com>
2022-11-25 11:27:24 +0700

    Update Port Script

--- command/deploy.sh
+++ command/deploy.sh
@@ -5,6 +5,6 @@ docker rm esignatures
 docker rmi esignatures
 git pull
 docker build -t esignatures .
-docker container create --name esignatures -e PORT=443 -e INSTANCE_ID="smartsign signatures" -p 443:443 esignatures
+docker container create --name esignatures -e PORT=2500 -e INSTANCE_ID="smartsign signatures" -p 2500:2500 esignatures
 docker container start esignatures
 
