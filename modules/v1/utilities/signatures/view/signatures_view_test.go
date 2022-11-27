2e5d5b15533a572820744aa90e4220f7357ecd40
Rizqi Wijaya <rizwijaya@github.com>
2022-11-27 07:23:27 +0700

    Create Unit Testing Invite Signatures in Signatures View

--- modules/v1/utilities/signatures/view/signatures_view_test.go
+++ modules/v1/utilities/signatures/view/signatures_view_test.go
@@ -145,3 +145,49 @@ func Test_signaturesView_SignDocuments(t *testing.T) {
 		})
 	}
 }
+
+func Test_signaturesview_InviteSignatures(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	serviceUser := m_serviceUser.NewMockService(ctrl)
+	serviceSignature := m_serviceSignature.NewMockService(ctrl)
+
+	cookies := "smartsign=MTY2OTQ3NDEyOHxEdi1CQkFFQ180SUFBUkFCRUFBQV9nRXdfNElBQmdaemRISnBibWNNQkFBQ2FXUUdjM1J5YVc1bkRCb0FHRFl6T0RCaU5XTmlaR001TXpoak5XWmtaamhsTm1KbVpRWnpkSEpwYm1jTUJnQUVjMmxuYmdaemRISnBibWNNQ3dBSmNtbDZkMmxxWVhsaEJuTjBjbWx1Wnd3R0FBUnVZVzFsQm5OMGNtbHVad3dPQUF4U2FYcHhhU0JYYVdwaGVXRUdjM1J5YVc1bkRBd0FDbkIxWW14cFkxOXJaWGtHYzNSeWFXNW5EQ3dBS2pCNFJFSkZOREUwTmpVeE0yTTVPVFEwTTJOR016SkRZVGhCTkRRNVpqVXlPRGRoWVVRMlpqa3hZUVp6ZEhKcGJtY01CZ0FFY205c1pRTnBiblFFQWdBRUJuTjBjbWx1Wnd3SUFBWndZWE56Y0dnR2MzUnlhVzVuRERnQU5rWkNTQ3RMYkZwd1dHOHhlVTFSUTNnMU9VVTBNRnAxYlROWVVHa3dSbmxWT1c1TFVsTkRNbWR4UkhVNGJteFNSMHM0TTJkRlp3PT189RnNnJPqyThKonDOKwf4QeHI-7SwOwzto9OciAktNLw="
+	test := []struct {
+		name       string
+		beforeTest func()
+	}{
+		{
+			name: "Test signaturesView Invite Signatures Success",
+			beforeTest: func() {
+				serviceUser.EXPECT().Logging("Mengakses undang orang lain untuk tanda tangan", "rizwijaya", gomock.Any(), gomock.Any()).Times(1)
+			},
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &signaturesview{
+				serviceSignature: serviceSignature,
+				serviceUser:      serviceUser,
+			}
+			if tt.beforeTest != nil {
+				tt.beforeTest()
+			}
+
+			got := w.InviteSignatures
+
+			router := NewRouter()
+			router.GET("/invite-signatures", got)
+			req, err := http.NewRequest("GET", "/invite-signatures", nil)
+			req.Header.Set("Cookie", cookies)
+			assert.NoError(t, err)
+			resp := httptest.NewRecorder()
+			router.ServeHTTP(resp, req)
+			responseData, err := ioutil.ReadAll(resp.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, http.StatusOK, resp.Code)
+			assert.Contains(t, string(responseData), "Undang untuk Tanda tangan - SmartSign")
+		})
+	}
+}
