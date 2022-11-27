a25cda23fa80f2dce6f34b9c51e345e28e62d738
Rizqi Wijaya <rizwijaya@github.com>
2022-11-27 13:36:55 +0700

    Create Unit Testing Change signatures in Signatures Handler

--- modules/v1/utilities/signatures/handler/http_handler_test.go
+++ modules/v1/utilities/signatures/handler/http_handler_test.go
@@ -64,7 +64,7 @@ func Test_signaturesHandler_AddSignatures(t *testing.T) {
 		serviceTest func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService)
 	}{
 		{
-			name:       "Success Add Signatures",
+			name:       "Test Add Signatures Success",
 			request:    `{"unique":"63322e432d405a140eb354e9","signature":"base64-pngimagefasnflanflasda"}`,
 			statusCode: http.StatusOK,
 			response:   `{"meta":{"message":"Success Add Signatures","code":200,"status":"success"},"data":null}`,
@@ -76,7 +76,7 @@ func Test_signaturesHandler_AddSignatures(t *testing.T) {
 			},
 		},
 		{
-			name:       "Request Format Failed",
+			name:       "Test Add Signatures, Input Not Valid",
 			request:    `{"unique":"63322e432d405a140eb354e9","signature":"base64-pngimagefasnflanflasda"`,
 			statusCode: 300,
 			response:   `{"meta":{"message":"unexpected EOF","code":300,"status":"error"},"data":null}`,
@@ -113,98 +113,106 @@ func Test_signaturesHandler_AddSignatures(t *testing.T) {
 	}
 }
 
-// func Test_signaturesHandler_ChangeSignatures(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
-// 	service := m_service.NewMockService(ctrl)
-// 	serviceUser := m_serviceUser.NewMockService(ctrl)
+func Test_signaturesHandler_ChangeSignatures(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	serviceSignature := m_serviceSignature.NewMockService(ctrl)
+	serviceUser := m_serviceUser.NewMockService(ctrl)
 
-// 	// mock := &signaturesHandler{
-// 	// 	signaturesService: service,
-// 	// 	serviceUser:       serviceUser,
-// 	// }
-// 	router := NewRouter()
-// 	//router.GET("/change-signatures/:sign_type", mock.ChangeSignatures)
+	cookies := "smartsign=MTY2OTQ3NDEyOHxEdi1CQkFFQ180SUFBUkFCRUFBQV9nRXdfNElBQmdaemRISnBibWNNQkFBQ2FXUUdjM1J5YVc1bkRCb0FHRFl6T0RCaU5XTmlaR001TXpoak5XWmtaamhsTm1KbVpRWnpkSEpwYm1jTUJnQUVjMmxuYmdaemRISnBibWNNQ3dBSmNtbDZkMmxxWVhsaEJuTjBjbWx1Wnd3R0FBUnVZVzFsQm5OMGNtbHVad3dPQUF4U2FYcHhhU0JYYVdwaGVXRUdjM1J5YVc1bkRBd0FDbkIxWW14cFkxOXJaWGtHYzNSeWFXNW5EQ3dBS2pCNFJFSkZOREUwTmpVeE0yTTVPVFEwTTJOR016SkRZVGhCTkRRNVpqVXlPRGRoWVVRMlpqa3hZUVp6ZEhKcGJtY01CZ0FFY205c1pRTnBiblFFQWdBRUJuTjBjbWx1Wnd3SUFBWndZWE56Y0dnR2MzUnlhVzVuRERnQU5rWkNTQ3RMYkZwd1dHOHhlVTFSUTNnMU9VVTBNRnAxYlROWVVHa3dSbmxWT1c1TFVsTkRNbWR4UkhVNGJteFNSMHM0TTJkRlp3PT189RnNnJPqyThKonDOKwf4QeHI-7SwOwzto9OciAktNLw="
 
-// 	tests := []struct {
-// 		name        string
-// 		statusCode  int
-// 		sign_type   string
-// 		sign_now    string
-// 		pages       string
-// 		serviceTest func(signatureService *m_service.MockService, signatureUser *m_serviceUser.MockService)
-// 	}{
-// 		{
-// 			name:       "Success Change Signatures in signature",
-// 			statusCode: 302,
-// 			sign_type:  "signature",
-// 			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
-// 			pages:      "/my-signatures",
-// 			serviceTest: func(signatureService *m_service.MockService, signatureUser *m_serviceUser.MockService) {
-// 				service.EXPECT().ChangeSignatures("signature", gomock.Any()).Return(nil)
-// 			},
-// 		},
-// 		{
-// 			name:       "Success Change Signatures in signature with data",
-// 			statusCode: 302,
-// 			sign_type:  "signature_data",
-// 			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
-// 			pages:      "/my-signatures",
-// 			serviceTest: func(signatureService *m_service.MockService, signatureUser *m_serviceUser.MockService) {
-// 				service.EXPECT().ChangeSignatures("signature_data", gomock.Any()).Return(nil)
-// 			},
-// 		},
-// 		{
-// 			name:       "Success Change Signatures in signature latin",
-// 			statusCode: 302,
-// 			sign_type:  "latin",
-// 			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
-// 			pages:      "/my-signatures",
-// 			serviceTest: func(signatureService *m_service.MockService, signatureUser *m_serviceUser.MockService) {
-// 				service.EXPECT().ChangeSignatures("latin", gomock.Any()).Return(nil)
-// 			},
-// 		},
-// 		{
-// 			name:       "Success Change Signatures in signature latin with data",
-// 			statusCode: 302,
-// 			sign_type:  "latin_data",
-// 			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
-// 			pages:      "/my-signatures",
-// 			serviceTest: func(signatureService *m_service.MockService, signatureUser *m_serviceUser.MockService) {
-// 				service.EXPECT().ChangeSignatures("latin_data", gomock.Any()).Return(nil)
-// 			},
-// 		},
-// 		{
-// 			name:       "Invalit input Signatures Type",
-// 			statusCode: 302,
-// 			sign_type:  "signature-nothing",
-// 			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
-// 			pages:      "/my-signatures",
-// 			serviceTest: func(signatureService *m_service.MockService, signatureUser *m_serviceUser.MockService) {
-// 				service.EXPECT().ChangeSignatures("latin", gomock.Any()).Return(nil)
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			//Testing Services Functions
-// 			if tt.serviceTest != nil {
-// 				tt.serviceTest(service, serviceUser)
-// 			}
-// 			//Testing Handler Functions
-// 			req, err := http.NewRequest("GET", "/change-signatures/"+tt.sign_type, nil)
-// 			assert.NoError(t, err)
-// 			response := httptest.NewRecorder()
-// 			router.ServeHTTP(response, req)
-// 			responseData, err := ioutil.ReadAll(response.Body)
-// 			assert.NoError(t, err)
+	tests := []struct {
+		name        string
+		statusCode  int
+		sign_type   string
+		sign_now    string
+		pages       string
+		serviceTest func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService)
+	}{
+		{
+			name:       "Change Signatures to signature success",
+			statusCode: 302,
+			sign_type:  "signature",
+			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
+			pages:      "/my-signatures",
+			serviceTest: func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService) {
+				serviceSignature.EXPECT().ChangeSignatures("signature", gomock.Any()).Times(1)
+				serviceUser.EXPECT().Logging("Mengganti tanda tangan ke signature", "rizwijaya", gomock.Any(), gomock.Any()).Times(1)
+			},
+		},
+		{
+			name:       "Change Signatures to signature data success",
+			statusCode: 302,
+			sign_type:  "signature_data",
+			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
+			pages:      "/my-signatures",
+			serviceTest: func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService) {
+				serviceSignature.EXPECT().ChangeSignatures("signature_data", gomock.Any()).Times(1)
+				serviceUser.EXPECT().Logging("Mengganti tanda tangan ke signature_data", "rizwijaya", gomock.Any(), gomock.Any()).Times(1)
+			},
+		},
+		{
+			name:       "Change Signatures to latin success",
+			statusCode: 302,
+			sign_type:  "latin",
+			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
+			pages:      "/my-signatures",
+			serviceTest: func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService) {
+				serviceSignature.EXPECT().ChangeSignatures("latin", gomock.Any()).Times(1)
+				serviceUser.EXPECT().Logging("Mengganti tanda tangan ke latin", "rizwijaya", gomock.Any(), gomock.Any()).Times(1)
+			},
+		},
+		{
+			name:       "Change Signatures to latin data success",
+			statusCode: 302,
+			sign_type:  "latin_data",
+			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
+			pages:      "/my-signatures",
+			serviceTest: func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService) {
+				serviceSignature.EXPECT().ChangeSignatures("latin_data", gomock.Any()).Times(1)
+				serviceUser.EXPECT().Logging("Mengganti tanda tangan ke latin_data", "rizwijaya", gomock.Any(), gomock.Any()).Times(1)
+			},
+		},
+		{
+			name:       "Change Signatures, random sign type",
+			statusCode: 302,
+			sign_type:  "signature-nothing",
+			sign_now:   "<a href=\"/my-signatures\">Found</a>.\n\n",
+			pages:      "/my-signatures",
+			serviceTest: func(signatureService *m_serviceSignature.MockService, signatureUser *m_serviceUser.MockService) {
+				serviceSignature.EXPECT().ChangeSignatures("latin", gomock.Any()).Times(1)
+				serviceUser.EXPECT().Logging("Mengganti tanda tangan ke signature-nothing", "rizwijaya", gomock.Any(), gomock.Any()).Times(1)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//Testing Services Functions
+			if tt.serviceTest != nil {
+				tt.serviceTest(serviceSignature, serviceUser)
+			}
+			w := &signaturesHandler{
+				serviceSignature: serviceSignature,
+				serviceUser:      serviceUser,
+			}
+			got := w.ChangeSignatures
+			router := NewRouter()
+			router.GET("/change-signatures/:sign_type", got)
+			//Testing Handler Functions
+			req, err := http.NewRequest("GET", "/change-signatures/"+tt.sign_type, nil)
+			req.Header.Set("Cookie", cookies)
+			assert.NoError(t, err)
+			response := httptest.NewRecorder()
+			router.ServeHTTP(response, req)
+			responseData, err := ioutil.ReadAll(response.Body)
+			assert.NoError(t, err)
 
-// 			assert.Equal(t, tt.statusCode, response.Code)
-// 			assert.Equal(t, string(responseData), tt.sign_now)
+			assert.Equal(t, tt.statusCode, response.Code)
+			assert.Equal(t, string(responseData), tt.sign_now)
 
-// 			location, err := response.Result().Location()
-// 			assert.NoError(t, err)
-// 			assert.Equal(t, location.Path, tt.pages)
-// 		})
-// 	}
-// }
+			location, err := response.Result().Location()
+			assert.NoError(t, err)
+			assert.Equal(t, location.Path, tt.pages)
+		})
+	}
+}
