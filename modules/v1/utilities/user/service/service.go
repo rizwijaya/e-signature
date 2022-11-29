f0ff7ce75759fb15818a738a13fbf59a57db83a4
Rizqi Wijaya <rizwijaya@github.com>
2022-11-29 17:32:46 +0700

    Create Library Cryptografi Encrypt an Decrypt

--- modules/v1/utilities/user/service/service.go
+++ modules/v1/utilities/user/service/service.go
@@ -1,38 +1,26 @@
 package service
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/sha256"
 	"e-signature/app/config"
 	"e-signature/modules/v1/utilities/user/models"
 	"e-signature/modules/v1/utilities/user/repository"
-	"encoding/base64"
+	pw "e-signature/pkg/crypto"
+	"e-signature/pkg/ipfs"
+	tm "e-signature/pkg/time"
 	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"os"
 	"time"
 
-	tm "e-signature/pkg/time"
-
 	"github.com/ethereum/go-ethereum/common"
-	shell "github.com/ipfs/go-ipfs-api"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type Service interface {
-	ConnectIPFS() *shell.Shell
 	UploadIPFS(path string) (string, error)
 	GetFileIPFS(hash string, output string, directory string) (string, error)
 	Login(input models.LoginInput) (models.ProfileDB, error)
 	CreateAccount(user models.User) (string, error)
 	//SaveImage(input models.RegisterUserInput, file *multipart.FileHeader) (string, error)
-	CreateKey(key string) []byte
 	Encrypt(data []byte, passphrase string) []byte
 	Decrypt(data []byte, passphrase string) []byte
 	EncryptFile(filename string, passphrase string) error
@@ -56,47 +44,12 @@ func NewService(repository repository.Repository) *service {
 	return &service{repository}
 }
 
-func (s *service) ConnectIPFS() *shell.Shell {
-	var sh *shell.Shell
-	conf, _ := config.Init()
-	sh = shell.NewShell(conf.IPFS.Host + ":" + conf.IPFS.Port)
-	return sh
-}
-
 func (s *service) UploadIPFS(path string) (string, error) {
-	//fmt.Printf("Adding %s on IPFS \n", filename)
-	sh := s.ConnectIPFS()
-	f, err := os.Open(path)
-	if err != nil {
-		log.Println(err)
-		return "", err
-	}
-	var r = f
-	cid, err := sh.Add(r)
-	if err != nil {
-		log.Println(err)
-		return "", err
-	}
-	defer f.Close()
-
-	err = os.Remove(path)
-	if err != nil {
-		//log.Println("The file could not be removed")
-		log.Println(err)
-		return "", err
-	}
-	return cid, nil
+	return ipfs.UploadIPFS(path)
 }
 
 func (s *service) GetFileIPFS(hash string, output string, directory string) (string, error) {
-	sh := s.ConnectIPFS()
-	outputName := directory + output
-	err := sh.Get(hash, outputName)
-	if err != nil {
-		log.Println(err)
-		return "", err
-	}
-	return outputName, nil
+	return ipfs.GetFileIPFS(hash, output, directory)
 }
 
 func (s *service) Login(input models.LoginInput) (models.ProfileDB, error) {
@@ -110,7 +63,7 @@ func (s *service) Login(input models.LoginInput) (models.ProfileDB, error) {
 		return user, errors.New("user not found")
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	err = pw.Compare(user.Password, password)
 	if err != nil {
 		log.Println(err)
 		return user, errors.New("password salah")
@@ -121,13 +74,13 @@ func (s *service) Login(input models.LoginInput) (models.ProfileDB, error) {
 
 func (s *service) CreateAccount(user models.User) (string, error) {
 	conf, _ := config.Init()
+	var err error
 	//Input Hash Password
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.MinCost)
+	user.PasswordHash, err = pw.GenerateHash(user.Password)
 	if err != nil {
 		log.Println(err)
 		return "", err
 	}
-	user.PasswordHash = string(hash)
 	//Generate account public key with passphrase user password
 	user, err = s.repository.GeneratePublicKey(user)
 	if err != nil {
@@ -181,86 +134,20 @@ func (s *service) CreateAccount(user models.User) (string, error) {
 // 	return cidr, nil
 // }
 
-func (s *service) CreateKey(key string) []byte {
-	hash := sha256.Sum256([]byte(key))
-	return hash[:]
-}
-
 func (s *service) Encrypt(data []byte, passphrase string) []byte {
-	key := s.CreateKey(passphrase)
-	block, _ := aes.NewCipher(key)
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		log.Println(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-
-	base64Cipher := make([]byte, base64.RawStdEncoding.EncodedLen(len(ciphertext)))
-	base64.RawStdEncoding.Encode(base64Cipher, ciphertext)
-	//return string(base64Cipher)
-	return base64Cipher
+	return pw.Encrypt(data, passphrase)
 }
 
 func (s *service) Decrypt(data []byte, passphrase string) []byte {
-	cipherText := make([]byte, base64.RawStdEncoding.DecodedLen(len(data)))
-	_, err := base64.RawStdEncoding.Decode(cipherText, data)
-	if err != nil {
-		return nil
-	}
-	key := s.CreateKey(passphrase)
-	//key := "791f13d3e2552bcf31c4f8d0e5d6a1ed"
-	//fmt.Println("key", key)
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := cipherText[:nonceSize], cipherText[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	return plaintext
+	return pw.Decrypt(data, passphrase)
 }
 
 func (s *service) EncryptFile(filename string, passphrase string) error {
-	b, err := ioutil.ReadFile(filename) //Read the target file
-	if err != nil {
-		fmt.Printf("Unable to open the input file!\n")
-		return err
-	}
-	ciphertext := s.Encrypt(b, passphrase)
-	err = ioutil.WriteFile(filename, ciphertext, 0644)
-	if err != nil {
-		fmt.Printf("Unable to create encrypted file!\n")
-		return err
-	}
-	//fmt.Println(ciphertext)
-	return nil
+	return pw.EncryptFile(filename, passphrase)
 }
 
 func (s *service) DecryptFile(filename string, passphrase string) error {
-	z, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Printf("Unable to open the input file!\n")
-		return err
-	}
-	result := s.Decrypt(z, passphrase)
-	//fmt.Printf("Decrypted file was created with file permissions 0777\n")
-	err = ioutil.WriteFile(filename, result, 0777)
-	if err != nil {
-		fmt.Printf("Unable to create decrypted file!\n")
-		return err
-	}
-	return nil
+	return pw.DecryptFile(filename, passphrase)
 }
 
 func (s *service) CheckUserExist(idsignature string) (string, error) {
--- pkg/crypto/crypto.go
+++ pkg/crypto/crypto.go
@@ -1,7 +1,15 @@
 package crypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"errors"
+	"io"
+	"io/ioutil"
+	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -21,6 +29,88 @@ func Compare(hash string, pw string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
 	if err != nil {
 		return errors.New("password salah")
+	}
+	return nil
+}
+
+func CreateKey(key string) []byte {
+	hash := sha256.Sum256([]byte(key))
+	return hash[:]
+}
+
+func Encrypt(data []byte, passphrase string) []byte {
+	key := CreateKey(passphrase)
+	block, _ := aes.NewCipher(key)
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		log.Println(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	base64Cipher := make([]byte, base64.RawStdEncoding.EncodedLen(len(ciphertext)))
+	base64.RawStdEncoding.Encode(base64Cipher, ciphertext)
+	//return string(base64Cipher)
+	return base64Cipher
+}
+
+func Decrypt(data []byte, passphrase string) []byte {
+	cipherText := make([]byte, base64.RawStdEncoding.DecodedLen(len(data)))
+	_, err := base64.RawStdEncoding.Decode(cipherText, data)
+	if err != nil {
+		return nil
+	}
+	key := CreateKey(passphrase)
+	//key := "791f13d3e2552bcf31c4f8d0e5d6a1ed"
+	//fmt.Println("key", key)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := cipherText[:nonceSize], cipherText[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return plaintext
+}
+
+func EncryptFile(filename string, passphrase string) error {
+	b, err := ioutil.ReadFile(filename) //Read the target file
+	if err != nil {
+		log.Println("Unable to open the input file!")
+		return err
+	}
+	ciphertext := Encrypt(b, passphrase)
+	err = ioutil.WriteFile(filename, ciphertext, 0644)
+	if err != nil {
+		log.Println("Unable to create encrypted file!")
+		return err
+	}
+	//fmt.Println(ciphertext)
+	return nil
+}
+
+func DecryptFile(filename string, passphrase string) error {
+	z, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Println("Unable to open the input file!")
+		return err
+	}
+	result := Decrypt(z, passphrase)
+	//fmt.Printf("Decrypted file was created with file permissions 0777\n")
+	err = ioutil.WriteFile(filename, result, 0777)
+	if err != nil {
+		log.Println("Unable to create decrypted file!")
+		return err
 	}
 	return nil
 }
