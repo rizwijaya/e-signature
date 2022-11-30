d5bca188bb49eb7996637b23b9d99fd57ddc9e5f
Rizqi Wijaya <rizwijaya@github.com>
2022-11-30 19:55:00 +0700

    Create Unit Testing TestInit and New Service in Signatures Service.

--- modules/v1/utilities/signatures/service/service_test.go
+++ modules/v1/utilities/signatures/service/service_test.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	os.Chdir("../../../../../")
+}
+
+func Test_service_NewService(t *testing.T) {
+	t.Run("Case 1: Success New Service", func(t *testing.T) {
+		serv := NewService(nil, nil, nil)
+		assert.NotNil(t, serv)
+	})
+}
