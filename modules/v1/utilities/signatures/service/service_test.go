fc9b8812ab640d5c907f57af05334275cf89d7f3
Rizqi Wijaya <rizwijaya@github.com>
2022-12-02 21:30:19 +0700

    Create Unit Testing Get Document No Signers in Signatures Service

--- modules/v1/utilities/signatures/service/service_test.go
+++ modules/v1/utilities/signatures/service/service_test.go
@@ -1618,3 +1618,93 @@ func Test_service_GetDocumentAllSign(t *testing.T) {
 		})
 	}
 }
+
+func Test_service_GetDocumentNoSigners(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	var err error
+	f := faketime.NewFaketime(2022, time.November, 27, 11, 30, 01, 0, time.UTC)
+	defer f.Undo()
+	f.Do()
+	location, err := time.LoadLocation("Asia/Jakarta")
+	assert.NoError(t, err)
+	times := time.Now().In(location)
+	timeSign := new(big.Int)
+	timeFormat := times.Format("15040502012006")
+	timeSign, _ = timeSign.SetString(timeFormat, 10)
+
+	test := []struct {
+		nameTest string
+		hash     string
+		docs     models.DocumentBlockchain
+		test     func(repo *m_repo.MockRepository, images *m_images.MockImages, docs *m_docs.MockDocuments)
+	}{
+		{
+			nameTest: "Get Document No Signers Case 1: Get Data Success",
+			hash:     "u798sc537106cb54272b66cda69f1bf51bd36a4c244e82419f9d725e15d9cc4b",
+			docs: models.DocumentBlockchain{
+				Document_id:    "0x1",
+				Creator:        common.HexToAddress("0xDBE4146513c99443cF32Ca8A449f5287aaD6f91a"),
+				Creator_string: "0xDBE4146513c99443cF32Ca8A449f5287aaD6f91a",
+				Creator_id:     "rizwijaya",
+				Metadata:       "sample_test.pdf",
+				Hash_ori:       "84637c537106cb54272b66cda69f1bf51bd36a4c244e82419f9d725e15d9cc4b",
+				Hash:           "u798sc537106cb54272b66cda69f1bf51bd36a4c244e82419f9d725e15d9cc4b",
+				IPFS:           "d9sj84msl02ndm93d8df4d2u43soj3bdsds",
+				State:          "2",
+				Mode:           "1",
+				Createdtime:    timeSign.String(),
+				Completedtime:  timeSign.String(),
+				Exist:          true,
+				Signers: models.Signers{
+					Sign_addr:     common.HexToAddress("0xDBE4146513c99443cF32Ca8A449f5287aaD6f91a"),
+					Sign_id:       "1",
+					Signers_id:    "rizwijaya",
+					Signers_hash:  "9f1bf51bd36a4c244e82419f9d725e15d9cc537106cb54u798sc272b66cda64b",
+					Signers_state: true,
+					Sign_time:     timeSign.String(),
+				},
+			},
+			test: func(repo *m_repo.MockRepository, images *m_images.MockImages, docs *m_docs.MockDocuments) {
+				repo.EXPECT().GetDocument("u798sc537106cb54272b66cda69f1bf51bd36a4c244e82419f9d725e15d9cc4b", "0x8101c772c3af62bb3096b5dd9dfd9b53cd50652e").Return(models.DocumentBlockchain{
+					Document_id:    "0x1",
+					Creator:        common.HexToAddress("0xDBE4146513c99443cF32Ca8A449f5287aaD6f91a"),
+					Creator_string: "0xDBE4146513c99443cF32Ca8A449f5287aaD6f91a",
+					Creator_id:     "rizwijaya",
+					Metadata:       "sample_test.pdf",
+					Hash_ori:       "84637c537106cb54272b66cda69f1bf51bd36a4c244e82419f9d725e15d9cc4b",
+					Hash:           "u798sc537106cb54272b66cda69f1bf51bd36a4c244e82419f9d725e15d9cc4b",
+					IPFS:           "d9sj84msl02ndm93d8df4d2u43soj3bdsds",
+					State:          "2",
+					Mode:           "1",
+					Createdtime:    timeSign.String(),
+					Completedtime:  timeSign.String(),
+					Exist:          true,
+					Signers: models.Signers{
+						Sign_addr:     common.HexToAddress("0xDBE4146513c99443cF32Ca8A449f5287aaD6f91a"),
+						Sign_id:       "1",
+						Signers_id:    "rizwijaya",
+						Signers_hash:  "9f1bf51bd36a4c244e82419f9d725e15d9cc537106cb54u798sc272b66cda64b",
+						Signers_state: true,
+						Sign_time:     timeSign.String(),
+					},
+				}).Times(1)
+			},
+		},
+	}
+
+	for _, tt := range test {
+		t.Run(tt.nameTest, func(t *testing.T) {
+			repo := m_repo.NewMockRepository(ctrl)
+			images := m_images.NewMockImages(ctrl)
+			docs := m_docs.NewMockDocuments(ctrl)
+			if tt.test != nil {
+				tt.test(repo, images, docs)
+			}
+			s := NewService(repo, images, docs)
+			output := s.GetDocumentNoSigners(tt.hash)
+			assert.Equal(t, tt.docs, output)
+		})
+	}
+}
