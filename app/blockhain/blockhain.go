e4072b68f564f6fe8c190a3bdbed34130c80d82c
Rizqi Wijaya <rizwijaya@github.com>
2022-12-13 14:50:14 +0700

    Create Contract Access without Deployed

--- app/blockhain/blockhain.go
+++ app/blockhain/blockhain.go
@@ -5,13 +5,19 @@ import (
 	"crypto/ecdsa"
 	"e-signature/app/config"
 	api "e-signature/app/contracts"
+	database "e-signature/app/databases"
+	"e-signature/modules/v1/utilities/user/models"
+	tm "e-signature/pkg/time"
+	"fmt"
 	"log"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GetAccountAuth(client *ethclient.Client, privateKeyAddress string) *bind.TransactOpts {
@@ -61,44 +67,48 @@ func Connect() *ethclient.Client {
 	return client
 }
 
+func NewContract(client *ethclient.Client, conf config.Conf) common.Address {
+	auth := GetAccountAuth(client, conf.Blockhain.Secret_key)
+	address, tx, _, err := api.DeployApi(auth, client)
+	if err != nil {
+		log.Println("Error Deploy API")
+		//log.Println(err)
+	}
+
+	// fmt.Println(address.Hex())
+	// fmt.Println("instance : ", instance)
+	fmt.Println("tx : ", tx.Hash().Hex())
+
+	db := database.Init(conf)
+	location, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		log.Println(err)
+	}
+	ctx := context.TODO()
+	trans := models.Transac{
+		Id:               primitive.NewObjectID(),
+		Address:          fmt.Sprintf("%v", address.Hex()),
+		Tx_hash:          fmt.Sprintf("%v", tx.Hash().Hex()),
+		Nonce:            auth.Nonce.String(),
+		Description:      "Aplikasi Berjalan, Membuat Kontrak",
+		Date_created:     time.Now().In(location),
+		Date_created_wib: tm.TanggalJam(time.Now().In(location)),
+	}
+	c := db.Collection("transactions")
+	_, err = c.InsertOne(ctx, &trans)
+	if err != nil {
+		log.Println(err)
+	}
+	return address
+}
+
 func Init(conf config.Conf) (*api.Api, *ethclient.Client) {
 	client := Connect()
-
-	//---------------Uncomment For New Deploy Smart Contract----------//
-	// auth := GetAccountAuth(client, conf.Blockhain.Secret_key)
-	// address, tx, _, err := api.DeployApi(auth, client)
-	// if err != nil {
-	// 	//log.Println("Error Deploy API")
-	// 	log.Println(err)
-	// }
-
-	// // fmt.Println(address.Hex())
-	// // fmt.Println("instance : ", instance)
-	// fmt.Println("tx : ", tx.Hash().Hex())
-
-	// db := database.Init(conf)
-	// location, err := time.LoadLocation("Asia/Jakarta")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// ctx := context.TODO()
-	// trans := models.Transac{
-	// 	Id:               primitive.NewObjectID(),
-	// 	Address:          fmt.Sprintf("%v", address.Hex()),
-	// 	Tx_hash:          fmt.Sprintf("%v", tx.Hash().Hex()),
-	// 	Nonce:            auth.Nonce.String(),
-	// 	Description:      "Aplikasi Berjalan, Membuat Kontrak",
-	// 	Date_created:     time.Now().In(location),
-	// 	Date_created_wib: tm.TanggalJam(time.Now().In(location)),
-	// }
-	// c := db.Collection("transactions")
-	// _, err = c.InsertOne(ctx, &trans)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	//------------End Uncomment For New Deploy Smart Contract----------//
-	address := common.HexToAddress(conf.Contract.Smart_contract) //Connect to Existing Contract
-	conn, err := api.NewApi(common.HexToAddress(address.Hex()), client)
+	//Deploy New Contract
+	address := NewContract(client, conf)
+	//Connect to Existing Contract
+	//address := common.HexToAddress(conf.Contract.Smart_contract)
+	conn, err := api.NewApi(address, client)
 	if err != nil {
 		log.Println("Error Connect to Smart Contract")
 		//log.Println(err)
